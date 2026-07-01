df8d3d49cc5979865efa56b5d5b314a9670d5443
Mohamed Walid <mo.waleedddd@gmail.com>
2026-07-01 13:27:14 +0300

    build a modular code

---
+++ src/strore_index.py
@@ -0,0 +1,64 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files, filter_to_minimal_docs, text_split, download_embeddings
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+extracted_data= load_pdf_files(data="data/")
+filter_data= filter_to_minimal_docs(extracted_data)
+text_chunks= text_split(filter_data)
+
+embedding = download_embeddings()
+
+pinecone_api_key = PINECONE_API_KEY
+pc= Pinecone(api_key=pinecone_api_key)
+
+
+index_name = "medical-chatbot"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name = index_name,
+        dimension=384,  # Dimension of the embeddings
+        metric= "cosine",  # Cosine similarity
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+
+index = pc.Index(index_name)
+
+
+
+
+# Load Existing index 
+from langchain_pinecone import PineconeVectorStore
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embedding
+)
+
+
+
+
+
+"""
+I made an index before so i loaded it from existing index, if you want to create a new index, use the below code and comment the above code.
+if you want to save a new index, use the following code:
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=texts_chunk,
+    embedding=embedding,
+    index_name=index_name
+)
+
+"""
